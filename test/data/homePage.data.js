56a43281e2a62035841bda68362bb5ad1566b066
Alexander Kiyko <avkiyko@gmail.com>
2020-04-02 16:11:05 -0400

    update homePage

---
+++ test/data/homePage.data.js
@@ -0,0 +1,9 @@
+const homePageData = {
+  pageHeader: 'Welcome to the-internet',
+  pageSubHeader: 'Available Examples',
+  pageFooter: 'Powered by Elemental Selenium',
+  requiredLinks: 44,
+};
+
+module.exports = homePageData;
+
--- test/specs/homePage.spec.js
+++ test/specs/homePage.spec.js
@@ -1,5 +1,6 @@
 import homePage from './../page/homePage.page';
 import elementUtil from './../util/elementUtil';
+import homePageData from './../data/homePage.data';
 
 describe('Home Page', function () {
   it('should load home page and verify correct url', function () {
@@ -8,18 +9,18 @@ describe('Home Page', function () {
   });
 
   it('should verify correct text page header', function () {
-    expect(elementUtil.elementText(homePage.pageHeader)).equal('Welcome to the-internet');
+    expect(elementUtil.elementText(homePage.pageHeader)).equal(homePageData.pageHeader);
   });
 
   it('should verify correct text page subheader', function () {
-    expect(elementUtil.elementText(homePage.subHeader)).equal('Available Examples');
+    expect(elementUtil.elementText(homePage.subHeader)).equal(homePageData.pageSubHeader);
   });
 
   it('should verify correct text page footer', function () {
-    expect(elementUtil.elementText(homePage.footer)).equal('Powered by Elemental Selenium');
+    expect(elementUtil.elementText(homePage.footer)).equal(homePageData.pageFooter);
   });
 
   it('should verify required amount of links on page', function () {
-    expect(homePage.linksAmount()).equal(44);
+    expect(homePage.linksAmount()).equal(homePageData.requiredLinks);
   });
 });
